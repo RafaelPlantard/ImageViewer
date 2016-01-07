d2bd2cef34c06a6a70bebe8f2a36cc4ebdcf21bf
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-07 11:33:31 -0200

    Initial Commit

---
+++ ImageViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageViewer
+//
+//  Created by Rafael Ferreira on 1/7/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
